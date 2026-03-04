596ae56db50bd2faec965c522565838cb9611762
Rodrigo <rodrigocbc21@gmail.com>
2026-03-04 13:42:03 +0000

    aaaa

--- main.c
+++ main.c
@@ -6,29 +6,12 @@
 /*   By: rodcaeta <rodcaeta@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 00:19:25 by rodcaeta          #+#    #+#             */
-/*   Updated: 2026/03/02 14:10:18 by rodcaeta         ###   ########.fr       */
+/*   Updated: 2026/03/03 11:05:34 by rodcaeta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/*void	print_stack(t_stack *s)
-{
-	t_node	*cur;
-	int		guard = 0;
-
-	if (!s || !s->root)
-		return (printf("(empty)\n"), (void)0);
-
-	cur = get_bot(s->root);
-	while (cur && guard++ < 100)
-	{
-		printf("Value[%d]    Index[%d]\n", cur->value, cur->index);
-		cur = cur->next;
-	}
-	if (cur)
-		printf("STOP:\n");
-}*/
 int	is_sorted(t_node *lst)
 {
 	while (lst && lst->next)
@@ -45,11 +28,12 @@ int main(int ac, char **av)
 	t_stack *stack_a;
 	t_stack *stack_b;
 	int		len;
+
 	if (ac < 2)
 		return (0);
 	stack_a = create_stack();
+	stack_fill(av, stack_a);
 	stack_b = create_stack();
-	stack_fill(av, stack_a, stack_b);
 	if (is_sorted(stack_a->root))
 	{
 		free_stack(stack_a);
--- src/node_utils.c
+++ src/node_utils.c
@@ -6,7 +6,7 @@
 /*   By: rodcaeta <rodcaeta@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 01:13:43 by rodcaeta          #+#    #+#             */
-/*   Updated: 2026/02/27 03:03:13 by rodcaeta         ###   ########.fr       */
+/*   Updated: 2026/03/02 16:08:38 by rodcaeta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,10 @@ void	add_top(t_stack *lst, t_node *new_top)
 {
 	if (!lst || !new_top)
 		return ;
-	
 	new_top->prev = NULL;
 	new_top->next = lst->root;
-	
 	if (lst->root)
 		lst->root->prev = new_top;
-	
 	lst->root = new_top;
 }
 
@@ -32,7 +29,6 @@ void	add_bot(t_stack *lst, t_node *new_bot)
 
 	if (!lst || !new_bot)
 		return ;
-	
 	if (!lst->root)
 	{
 		lst->root = new_bot;
@@ -40,11 +36,9 @@ void	add_bot(t_stack *lst, t_node *new_bot)
 		new_bot->next = NULL;
 		return ;
 	}
-	
 	last = lst->root;
 	while (last->next)
 		last = last->next;
-	
 	last->next = new_bot;
 	new_bot->prev = last;
 	new_bot->next = NULL;
@@ -56,16 +50,12 @@ t_node	*pop_top(t_stack *stack)
 
 	if (!stack || !stack->root)
 		return (NULL);
-	
 	pop = stack->root;
 	stack->root = pop->next;
-	
 	if (stack->root)
 		stack->root->prev = NULL;
-	
 	pop->next = NULL;
 	pop->prev = NULL;
-	
 	return (pop);
 }
 
@@ -75,16 +65,13 @@ t_node	*pop_bot(t_stack *stack)
 
 	if (!stack || !stack->root)
 		return (NULL);
-	
 	pop = stack->root;
 	while (pop->next)
 		pop = pop->next;
-	
 	if (pop->prev)
 		pop->prev->next = NULL;
 	else
 		stack->root = NULL;
-	
 	pop->prev = NULL;
 	pop->next = NULL;
 	
